63547887d9861117b4613e8727777b9901377c32
Ismael <ismaelmartin302@gmail.com>
2024-12-19 18:21:51 +0100

    changing  log message

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  Logger.log(`App running in port: http://localhost:${process.env.PORT ?? 3000}`)
+  console.log(`\nApp running in port: http://localhost:${process.env.PORT ?? 3000}`)
 }
 bootstrap();
