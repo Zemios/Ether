3c2b1664367b13af717497aa9de9622fed1e95bc
isma <ismaelmartin302@gmail.com>
2024-12-21 14:05:02 +0100

    created modules

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,6 +2,17 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { UserModule } from './user/user.module';
 import { RouterModule } from '@nestjs/core'; // Importa RouterModule
+import { CommentModule } from './comment/comment.module';
+import { CourseModule } from './course/course.module';
+import { LessonModule } from './lesson/lesson.module';
+import { LikeModule } from './like/like.module';
+import { ModuleModule } from './module/module.module';
+import { NewsModule } from './news/news.module';
+import { PostModule } from './post/post.module';
+import { ProjectModule } from './project/project.module';
+import { ProjectCollaboratorModule } from './project-collaborator/project-collaborator.module';
+import { UserProgressModule } from './user-progress/user-progress.module';
+import { UserProjectModule } from './user-project/user-project.module';
 
 @Module({
   controllers: [ApiController],
@@ -13,14 +24,68 @@ import { RouterModule } from '@nestjs/core'; // Importa RouterModule
         module: ApiModule,
         children: [
           {
-            path: 'user', // Prefijo 'user' dentro de 'api'
+            path: 'user',
             module: UserModule,
           },
-          // Agrega más módulos aquí, si es necesario (like, comment, etc.)
+          {
+            path: 'comment',
+            module: CommentModule,
+          },
+          {
+            path: 'course',
+            module: CourseModule,
+          },
+          {
+            path: 'lesson',
+            module: LessonModule,
+          },
+          {
+            path: 'like',
+            module: LikeModule,
+          },
+          {
+            path: 'module',
+            module: ModuleModule,
+          },
+          {
+            path: 'news',
+            module: NewsModule,
+          },
+          {
+            path: 'post',
+            module: PostModule,
+          },
+          {
+            path: 'project',
+            module: ProjectModule,
+          },
+          {
+            path: 'project-collaborator',
+            module: ProjectCollaboratorModule,
+          },
+          {
+            path: 'user-progress',
+            module: UserProgressModule,
+          },
+          {
+            path: 'user-project',
+            module: UserProjectModule,
+          },
         ],
       },
     ]),
-    UserModule, // Importa los módulos de `user` y demás submódulos aquí
+    UserModule,
+    CommentModule,
+    CourseModule,
+    LessonModule,
+    LikeModule,
+    ModuleModule,
+    NewsModule,
+    PostModule,
+    ProjectModule,
+    ProjectCollaboratorModule,
+    UserProgressModule,
+    UserProjectModule, // Importa los módulos de `user` y demás submódulos aquí
   ],
 })
 export class ApiModule { }
