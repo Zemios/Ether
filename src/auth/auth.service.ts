1e99f6d3645d11ac22d47395c1fc0a0ca3e1ea9c
isma <ismaelmartin302@gmail.com>
2024-12-29 13:37:35 +0100

    `Added validation and DTO for user registration`

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly userService: UsersService) { }
-    login() {
-        return 'login'
+    register(registerDto: RegisterDto) {
+        return 'register'
     }
 
-    register() {
-        return 'register'
+    login() {
+        return 'login'
     }
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,5 +1,19 @@
+import { Transform } from "class-transformer";
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
+
 export class RegisterDto {
+    @Transform(({ value }) => value.trim())
+    @IsString()
+    @MaxLength(50)
     name: string;
+
+    @IsEmail()
+    @MaxLength(255)
     email: string;
+
+    @Transform(({ value }) => value.trim())
+    @IsString()
+    @MaxLength(255)
+    @MinLength(8)
     password: string;
 }
