347c2740bcb329a0a4439afcc5a119f7bd68ad66
isma <ismaelmartin302@gmail.com>
2024-12-29 15:30:50 +0100

    destructure user request

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,14 +5,14 @@ import { RegisterDto } from './dto/register.dto';
 @Injectable()
 export class AuthService {
     constructor(private readonly userService: UsersService) { }
-    async register(registerDto: RegisterDto): Promise<any> {
-        const user = await this.userService.findOneByEmail(registerDto.email)
+    async register({ name, email, password }: RegisterDto): Promise<any> {
+        const user = await this.userService.findOneByEmail(email)
 
         if (user) {
             throw new BadRequestException('Email already exists');
         }
 
-        return await this.userService.create(registerDto);
+        return await this.userService.create({ name, email, password });
     }
 
     login() {
