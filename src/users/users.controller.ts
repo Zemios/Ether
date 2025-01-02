d0f965c4b250a1d344d7142fc17c77e0e8ca1284
isma <ismaelmartin302@gmail.com>
2025-01-02 14:45:42 +0100

    `Removed RoleGuard and NeedRole decorator from UsersController`

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,7 @@
-import { NeedRole } from 'src/auth/decorators/need-role.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
-import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
-import { RoleGuard } from 'src/auth/guards/role.guard';
-import { Role } from 'src/auth/enums/role.enum';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -14,8 +11,6 @@ export class UsersController {
     return this.usersService.findAll(); // TODO: Proteger ruta (SOLO ADMIN(?) o que al menos no devuelva las contraseñas ni las id??)
   }
   @Get('/:id')
-  @NeedRole(Role.USER)
-  @UseGuards(RoleGuard)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id)); // TODO: Proteger ruta (SOLO ADMIN(?)) o que no de contraseña por lo menos
   }
@@ -26,15 +21,11 @@ export class UsersController {
   }
 
   @Put('/:id')
-  @NeedRole(Role.USER)
-  @UseGuards(RoleGuard)
   update(@Param('id') id: string, @Body() updateUserDto: CreateUserDto): Promise<User> {
     return this.usersService.update(parseInt(id), updateUserDto); // TODO: Proteger ruta (SOLO EL MISMO USUARIO SE PUEDE ACTUALIZAR A SI MISMO, O UN ADMIN)
   }
 
   @Delete('/:id')
-  @NeedRole(Role.USER)
-  @UseGuards(RoleGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id)); // TODO: Proteger ruta (SOLO ADMIN(?) O EL PROPIO USUARIO)
   }
