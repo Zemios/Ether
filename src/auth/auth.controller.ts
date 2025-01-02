9e3215719168a7bf9ea26b3557173bb4789a8633
isma <ismaelmartin302@gmail.com>
2025-01-02 13:18:20 +0100

    `Added AuthGuard and test endpoint to auth.controller.ts`

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +17,13 @@ export class AuthController {
     @Post('login')
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto)
+    }
+
+
+    // Ejemplo
+    @Get('test')
+    @UseGuards(AuthGuard)
+    test() {
+        return 'Has accedido a informacion privada!!!! ⚠⚠☢🚮🏧🚹🛑🛑🛑⛔⛔'
     }
 }
