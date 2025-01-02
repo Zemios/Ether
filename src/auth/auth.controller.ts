3b5aa38ea6be0e4b00f21962de0f566a69dccc7f
isma <ismaelmartin302@gmail.com>
2025-01-02 13:37:41 +0100

    `Added AuthGuard and profile endpoint to AuthController`

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +17,11 @@ export class AuthController {
     @Post('login')
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto)
+    }
+
+    @Get('profile')
+    @UseGuards(AuthGuard)
+    profile(@Request() req) {
+        return req.user
     }
 }
